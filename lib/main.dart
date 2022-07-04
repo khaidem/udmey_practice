23e971ab0ec76415b77fedef1e1a38622b28cbea
khaidem <khaidemdenish@gmail.com>
2022-07-04 17:29:51 +0530

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:practice_app/ShoppingApp/example/home_screem.dart';
 
+import 'ShoppingApp/example/extract_arguments_screen.dart';
+
 void main() {
-  runApp(const HomeScreen());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        ExtractArgumentsScreen.routeName: (context) =>
+            const ExtractArgumentsScreen(),
+      },
+      title: 'Navigation with Arguments',
+      home: const HomeScreen(),
+    );
+  }
 }
